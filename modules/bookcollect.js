a8f11b3523133f1a952289d4bf5aa1c4c936ed24
Christian Nualart <nualart@Christians-Mac-mini.local>
2023-08-14 16:26:18 -0600

    Correct eslint errors

--- modules/bookcollect.js
+++ modules/bookcollect.js
@@ -1,20 +1,20 @@
 export default class BookCollect {
- constructor() {
-   this.books = [];
- }
+  constructor() {
+    this.books = [];
+}
 
- removeBook = (index) => {
-   this.books = this.books.filter((item, i) => i !== index);
-   localStorage.setItem('Books', JSON.stringify(this.books));
-   window.location.reload();
- }
+  removeBook = (index) => {
+    this.books = this.books.filter((item, i) => i !== index);
+    localStorage.setItem('Books', JSON.stringify(this.books));
+    window.location.reload();
+  }
 
- addBook = (BookName, Author) => {
-   const book = {
-     name: BookName,
-     author: Author,
-   };
-   this.books.push(book);
-   localStorage.setItem('Books', JSON.stringify(this.books));
- }
+  addBook = (BookName, Author) => {
+    const book = {
+      name: BookName,
+      author: Author,
+    };
+    this.books.push(book);
+    localStorage.setItem('Books', JSON.stringify(this.books));
+  }
 }
